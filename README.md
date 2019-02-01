05711bc8392939d0609711ca1ee6706db01b12b2
lizhi <lizhi1026@126.com>
2019-02-01 16:36:37 +0800

    files copy

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ```
 sys.path[0]         #源文件父路径
-print sys.argv[0]   #源文件路径
+sys.argv[0]         #源文件路径
 os.getcwd()         #获取当前工作目录路径
+
+shutil.copytree(from_dir, to_dir) #拷贝目录，to_dir的路径必须不存在，此处的处理方式是如果这个路径文件已存在，删除后新建
 ```
--- codeGeneration.py
+++ codeGeneration.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
-import sys
-import os
+import os,sys,shutil
 
 root = os.getcwd() #获取当前工作目录路径
 path = sys.path[0] #源文件路径
 
 
-def file_name(file_dir):
+def file_replace(file_dir,moudleName):
     for _, dirs, files in os.walk(file_dir):
         print "-----------"
         print dirs   #os.walk()所在目录的所有目录名
         print files  #os.walk()所在目录的所有非目录文件名
         print " "
 
+def files_copy(from_dir,to_dir,moudleName):
+    dir = to_dir + '/' + moudleName.capitalize()
+
+    if os.path.exists(dir): #目录已存在则删除目录
+        shutil.rmtree(dir)
+    
+    shutil.copytree(from_dir, dir)
+    file_replace(dir,moudleName)
 
 
 def main():
@@ -23,9 +30,7 @@ def main():
     for moudleName in moudles:
         moudle = moudleName.strip()
         if len(moudle) > 3:
-            file_name(path+'/Template')
-
-    
+            files_copy(path+'/Template', root, moudle)
 
 
 if __name__ == '__main__':
